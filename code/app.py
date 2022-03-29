a0d85ab43ef26356ef5b263e0e9bafed23d78394
unknown <459461160@qq.com>
2022-03-29 23:49:20 +0800

    网站后端能运行

--- code/app.py
+++ code/app.py
@@ -4,10 +4,26 @@
 # @File : app.py
 # @Software : PyCharm
 
-from flask import Flask
+from flask import Flask, request, render_template, make_response, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = b'auyang'
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if 'username' in session:
+        res = make_response(render_template('welcome.html', method=request.method, data=request.data.decode()))
+        res.set_cookie('username', 'the username')
+        return res
+    else:
+        app.logger.error("user not login")
+        return redirect(url_for('login'))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        app.logger.debug("user login")
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
