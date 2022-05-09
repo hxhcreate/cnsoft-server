f47d50667b6553e3a2599027d3c2dc37f2802e26
金阳 徐 <459461160@qq.com>
2022-05-09 13:14:32 +0800

    能访问静态文件

--- code/vue/eladmin-web/src/api/login.js
+++ code/vue/eladmin-web/src/api/login.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(username, password, code, uuid) {
   return request({
-    url: 'auth/login',
+    url: 'login',
     method: 'post',
     data: {
       username,
@@ -15,21 +15,21 @@ export function login(username, password, code, uuid) {
 
 export function getInfo() {
   return request({
-    url: 'auth/info',
+    url: 'info',
     method: 'get'
   })
 }
 
 export function getCodeImg() {
   return request({
-    url: 'auth/code',
+    url: 'code',
     method: 'get'
   })
 }
 
 export function logout() {
   return request({
-    url: 'auth/logout',
+    url: 'logout',
     method: 'delete'
   })
 }
--- code/vue/eladmin-web/src/views/login.vue
+++ code/vue/eladmin-web/src/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login" :style="'background-image:url(auth/' + Background + ');'">
+  <div class="login" :style="'background-image:url(' + Background + ');'">
     <el-form
       ref="loginForm"
       :model="loginForm"
@@ -53,7 +53,7 @@
           />
         </el-input>
         <div class="login-code">
-          <img :src="codeUrl" @click="getCode">
+          <img :src="codeUrl" @click="getCode" />
         </div>
       </el-form-item>
       <el-checkbox v-model="loginForm.rememberMe" style="margin: 0 0 25px 0">
@@ -84,85 +84,85 @@
 </template>
 
 <script>
-import { encrypt } from '@/utils/rsaEncrypt'
-import Config from '@/settings'
-import { getCodeImg } from '@/api/login'
-import Cookies from 'js-cookie'
-import qs from 'qs'
-import Background from '@/assets/images/background.jpeg'
+import { encrypt } from "@/utils/rsaEncrypt";
+import Config from "@/settings";
+import { getCodeImg } from "@/api/login";
+import Cookies from "js-cookie";
+import qs from "qs";
+import Background from "@/assets/images/background.jpeg";
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
       Background: Background,
-      codeUrl: '',
-      cookiePass: '',
+      codeUrl: "",
+      cookiePass: "",
       loginForm: {
-        username: 'admin',
-        password: '123456',
+        username: "admin",
+        password: "123456",
         rememberMe: false,
-        code: '',
-        uuid: ''
+        code: "",
+        uuid: "",
       },
       loginRules: {
         username: [
-          { required: true, trigger: 'blur', message: '用户名不能为空' }
+          { required: true, trigger: "blur", message: "用户名不能为空" },
         ],
         password: [
-          { required: true, trigger: 'blur', message: '密码不能为空' }
+          { required: true, trigger: "blur", message: "密码不能为空" },
         ],
         code: [
-          { required: true, trigger: 'change', message: '验证码不能为空' }
-        ]
+          { required: true, trigger: "change", message: "验证码不能为空" },
+        ],
       },
       loading: false,
-      redirect: undefined
-    }
+      redirect: undefined,
+    };
   },
   watch: {
     $route: {
-      handler: function(route) {
-        const data = route.query
+      handler: function (route) {
+        const data = route.query;
         if (data && data.redirect) {
-          this.redirect = data.redirect
-          delete data.redirect
-          if (JSON.stringify(data) !== '{}') {
+          this.redirect = data.redirect;
+          delete data.redirect;
+          if (JSON.stringify(data) !== "{}") {
             this.redirect =
-              this.redirect + '&' + qs.stringify(data, { indices: false })
+              this.redirect + "&" + qs.stringify(data, { indices: false });
           }
         }
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   created() {
     // 获取验证码
-    this.getCode()
+    this.getCode();
     // 获取用户名密码等Cookie
-    this.getCookie()
+    this.getCookie();
     // token 过期提示
-    this.point()
+    this.point();
   },
   methods: {
     getCode() {
       getCodeImg().then((res) => {
-        this.codeUrl = res.img
-        this.loginForm.uuid = res.uuid
-      })
+        this.codeUrl = res.img;
+        this.loginForm.uuid = res.uuid;
+      });
     },
     getCookie() {
-      const username = Cookies.get('username')
-      let password = Cookies.get('password')
-      const rememberMe = Cookies.get('rememberMe')
+      const username = Cookies.get("username");
+      let password = Cookies.get("password");
+      const rememberMe = Cookies.get("rememberMe");
       // 保存cookie里面的加密后的密码
-      this.cookiePass = password === undefined ? '' : password
-      password = password === undefined ? this.loginForm.password : password
+      this.cookiePass = password === undefined ? "" : password;
+      password = password === undefined ? this.loginForm.password : password;
       this.loginForm = {
         username: username === undefined ? this.loginForm.username : username,
         password: password,
         rememberMe: rememberMe === undefined ? false : Boolean(rememberMe),
-        code: ''
-      }
+        code: "",
+      };
     },
     handleLogin() {
       this.$refs.loginForm.validate((valid) => {
@@ -171,58 +171,58 @@ export default {
           password: this.loginForm.password,
           rememberMe: this.loginForm.rememberMe,
           code: this.loginForm.code,
-          uuid: this.loginForm.uuid
-        }
+          uuid: this.loginForm.uuid,
+        };
         if (user.password !== this.cookiePass) {
           // user.password = encrypt(user.password);
         }
         if (valid) {
-          this.loading = true
+          this.loading = true;
           if (user.rememberMe) {
-            Cookies.set('username', user.username, {
-              expires: Config.passCookieExpires
-            })
-            Cookies.set('password', user.password, {
-              expires: Config.passCookieExpires
-            })
-            Cookies.set('rememberMe', user.rememberMe, {
-              expires: Config.passCookieExpires
-            })
+            Cookies.set("username", user.username, {
+              expires: Config.passCookieExpires,
+            });
+            Cookies.set("password", user.password, {
+              expires: Config.passCookieExpires,
+            });
+            Cookies.set("rememberMe", user.rememberMe, {
+              expires: Config.passCookieExpires,
+            });
           } else {
-            Cookies.remove('username')
-            Cookies.remove('password')
-            Cookies.remove('rememberMe')
+            Cookies.remove("username");
+            Cookies.remove("password");
+            Cookies.remove("rememberMe");
           }
           this.$store
-            .dispatch('Login', user)
+            .dispatch("Login", user)
             .then(() => {
-              this.loading = false
-              this.$router.push({ path: this.redirect || '/' })
+              this.loading = false;
+              this.$router.push({ path: this.redirect || "/" });
             })
             .catch(() => {
-              this.loading = false
-              this.getCode()
-            })
+              this.loading = false;
+              this.getCode();
+            });
         } else {
-          console.log('error submit!!')
-          return false
+          console.log("error submit!!");
+          return false;
         }
-      })
+      });
     },
     point() {
-      const point = Cookies.get('point') !== undefined
+      const point = Cookies.get("point") !== undefined;
       if (point) {
         this.$notify({
-          title: '提示',
-          message: '当前登录状态已过期，请重新登录！',
-          type: 'warning',
-          duration: 5000
-        })
-        Cookies.remove('point')
+          title: "提示",
+          message: "当前登录状态已过期，请重新登录！",
+          type: "warning",
+          duration: 5000,
+        });
+        Cookies.remove("point");
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
